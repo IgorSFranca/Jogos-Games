bd33184b8152cb42fc561161ad4a4e709ea3486f
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-24 08:12:33 -0400

    Cabeçalho e Jogo inicial
    Atualizado cabeçalho com um guia de índices para o usuário e foi incluído o laço para inicializar o jogo

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -14,27 +14,35 @@ void cabecalho(){
     printf("+          ~~*~~           +\n");
     printf("+   ~# JOGO DA VELHA #~    +\n");
     printf("+          ~~*~~           +\n");
+    printf("++++++++++++++++++++++++++++\n");
+    printf("+          ~~*~~           +\n");
+    printf("+   INDICES GUIA DO JOGO   +\n");
+    printf("+        1 | 2 | 3         +\n");
+    printf("+        4 | 5 | 6         +\n");
+    printf("+        7 | 8 | 9         +\n");
+    printf("+          ~~*~~           +\n");
     printf("++++++++++++++++++++++++++++\n\n");
-    
 }
 
 int main (){
-    int i, j, linha=3, coluna=3;
+    int i, j, linha=3, coluna=3, game;
     char jogo[linha][coluna];
 
-    cabecalho();
-    for (i=0; i<linha; i++){//Inicializar o jogo
-        for (j=0; j<coluna; j++){
-            jogo[i][j] = '*';
+    do{
+        cabecalho();
+        for (i=0; i<linha; i++){//Inicializar o jogo
+            for (j=0; j<coluna; j++){
+                jogo[i][j] = '*';
+            }
         }
-    }
-    for (i=0; i<linha; i++){//Mostrar o jogo inicial vazio
-        printf("\n");
-        for (j=0; j<coluna; j++){
-            printf("%c ", jogo[i][j]);
+        for (i=0; i<linha; i++){//Mostrar o jogo inicial vazio
+            printf("\n");
+            for (j=0; j<coluna; j++){
+                printf("%c ", jogo[i][j]);
+            }
         }
-    }
-    printf("\n\n");
+        printf("\n\n");    
+    } while (game != 0);
     system ("pause");
     return 0;
 }
