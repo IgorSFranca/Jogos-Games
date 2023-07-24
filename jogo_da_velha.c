1b3bde439db5fd441b1f5d095610e4ff07b8fc58
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-24 07:16:45 -0400

    Create jogo_da_velha.c

---
+++ jogo_da_velha.c
@@ -0,0 +1,17 @@
+/*
+ * Nome do arquivo: jogo_da_velha.c
+ * Descrição: Jogo infanto-juvenil conhecido como Jogo da Velha, onde 2 usuário jogam, cada um precisa fazer 3 pontos em uma linha reta horizontal, 
+ * vertical ou perpendicular.
+ * Autor: Igor França
+ * Data de criação: 24 de julho de 2023
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+    system ("pause");
+    return 0;
+}
