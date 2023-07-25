35c65cce86bcb5e4cf41e259d0157f90b35df830
Igor França <125696858+IgorSFranca@users.noreply.github.com>
2023-07-25 19:30:54 -0400

    Create jo-ken-po.c

---
+++ jo-ken-po.c
@@ -0,0 +1,15 @@
+/*
+ * Nome do arquivo: jo-ken-po.c
+ * Descrição: Jogo do Jokenpô
+ * Autor: Igor França
+ * Data de criação: 25 de julho de 2023
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+  system("pause");
+  return 0;
+}
