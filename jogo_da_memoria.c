5da7f375c0a41e47270a66dec45590635d3ca75e
Igor França ∴ <125696858+IgorSFranca@users.noreply.github.com>
2023-07-27 14:01:12 -0400

    Arquivo inicial
    Criação do arquivo

---
+++ jogo_da_memoria.c
@@ -0,0 +1,24 @@
+/*
+ * Nome do arquivo: meu_programa.c
+ * Descrição: Programa para realizar uma determinada tarefa.
+ * Autor: Seu Nome
+ * Data de criação: 22 de julho de 2023
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void cabecalho(){
+    printf(" +++++++++++++++++++++++++++++\n");
+    printf(" +           ~~*~~           +\n");
+    printf(" + ~#   JOGO DA MEMORIA   #~ +\n");
+    printf(" +           ~~*~~           +\n");
+    printf(" +++++++++++++++++++++++++++++\n\n");
+}
+
+int main (){
+    cabecalho();
+
+
+    return 0;
+}
